a67bd60dc07465a4240b977e5903adc237971818
st3v1sh <st3v1sh@gmail.com>
2025-07-26 16:21:20 -0400

    improve first load message

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const sortComparisons = {
 const grantPermissionSVG = '<svg width="20" height="20" viewBox="0 0 20 20" focusable="false" aria-hidden="true" role="presentation" fill="var(--text-primary)"><path fill-rule="evenodd" d="M7 2a4 4 0 0 0-1.015 7.87A1.334 1.334 0 0 1 4.667 11 2.667 2.667 0 0 0 2 13.667V18h2v-4.333c0-.368.298-.667.667-.667A3.32 3.32 0 0 0 7 12.047 3.32 3.32 0 0 0 9.333 13c.369 0 .667.299.667.667V18h2v-4.333A2.667 2.667 0 0 0 9.333 11c-.667 0-1.22-.49-1.318-1.13A4.002 4.002 0 0 0 7 2zM5 6a2 2 0 1 0 4 0 2 2 0 0 0-4 0z" clip-rule="evenodd"></path><path d="m15 7 3 3-3 3v-2h-3V9h3V7z"></path></svg>';
 const errorHTML = {
     communication: () => '<p style="color: var(--color-error); font-weight: bold; text-align: center; font-size: 1.2rem; padding: 2rem;">Error communicating with Twitch. Please try again.</p>',
-    noPermission: () => `<p style="color: var(--color-error); font-weight: bold; text-align: center; font-size: 1.2rem; padding: 2rem;">This extension requires your username.<br><br>Use the "Grant Permissions" button to access your inventory.<br><br>It looks like this:</p>${grantPermissionSVG}`,
-    askPermission: () => `<p style="color: var(--color-error); font-weight: bold; text-align: center; font-size: 1.2rem; padding: 2rem;">Use the "Grant Permissions" button to access your inventory.<br><br>It looks like this:</p>${grantPermissionSVG}`,
+    noPermission: () => `<p style="color: var(--color-error); font-weight: bold; text-align: center; font-size: 1.2rem; padding: 2rem;">This extension requires your username.<br><br>Use the "Grant Permissions" button to authenticate.<br><br>It looks like this:</p>${grantPermissionSVG}`,
+    askPermission: () => `<p style="color: var(--color-error); font-weight: bold; text-align: center; font-size: 1.2rem; padding: 2rem;">Use the "Grant Permissions" button to authenticate.<br><br>It looks like this:</p>${grantPermissionSVG}`,
     other: (error) => `<p style="color: var(--color-error); font-weight: bold; text-align: center; font-size: 1.2rem; padding: 2rem;">${error.message}</p>`,
 }
 
@@ -455,4 +455,4 @@ window.Twitch.ext.onAuthorized((auth) => {
 pityPointsDisplay.textContent = '...';
 equippedTailDisplay.textContent = '...';
 freePullsDisplay.textContent = '...';
-inventoryPanel.innerHTML = `<p style="text-align: center; font-size: 1.2rem; padding: 2rem;">Waiting for Twitch data...</p>`;
+inventoryPanel.innerHTML = `<p style="text-align: center; font-size: 1.2rem; padding: 2rem;">Waiting for Twitch authentication...<br><br>If authentication was not automatically triggered, use the "Grant Permissions" button to authenticate.<br><br>It looks like this:<br><br>${grantPermissionSVG}</p>`;
