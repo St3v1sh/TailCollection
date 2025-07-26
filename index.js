a195bc0e0ae924af476ecb3bfe93e6202b8d0030
st3v1sh <st3v1sh@gmail.com>
2025-07-26 13:51:42 -0400

    add helix api calls

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const sortComparisons = {
 const errorHTML = {
     communication: () => '<p style="color: #ff8a8a; font-weight: bold; text-align: center; font-size: 1.2rem; padding: 2rem;">Error communicating with Twitch.</p>',
     noPermission: () => '<p style="color: #ff8a8a; font-weight: bold; text-align: center; font-size: 1.2rem; padding: 2rem;">This extension requires permission to access your username.</p>',
+    askPermission: () => '<p style="color: #ff8a8a; font-weight: bold; text-align: center; font-size: 1.2rem; padding: 2rem;">Use the "Grant Permission" button to allow this extension to access your username.</p>',
     other: (error) => `<p style="color: #ff8a8a; font-weight: bold; text-align: center; font-size: 1.2rem; padding: 2rem;">${error.message}</p>`,
 }
 
@@ -461,13 +462,19 @@ toggleStatsButton.addEventListener('click', () => {
 });
 
 // --- Twitch extension ---
-function fetchTwitchUsername(userId, token, clientId) {
-    const twitchApiURL = `https://api.twitch.tv/helix/users?id=${userId}`;
+function fetchTwitchUsername(helixToken, clientId) {
+    if (!window.Twitch.ext.viewer.isLinked) {
+        const innerHTML = errorHTML.askPermission(); const enableButtons = true;
+        displayError(innerHTML, enableButtons);
+        return;
+    }
+    const viewerId = window.Twitch.ext.viewer.id;
+    const twitchApiURL = `https://api.twitch.tv/helix/users/?id=${viewerId}`;
 
     fetch(twitchApiURL, {
         method: 'GET',
         headers: {
-            'Authorization': `Bearer ${token}`,
+            'Authorization': `Extension ${helixToken}`,
             'Client-Id': clientId,
         }
     })
@@ -487,6 +494,15 @@ function fetchTwitchUsername(userId, token, clientId) {
             displayError(innerHTML, enableButtons);
         });
 }
+
+window.Twitch.ext.onAuthorized((auth) => {
+    if (auth.userId.startsWith('A')) {
+        const innerHTML = errorHTML.noPermission(); const enableButtons = true;
+        displayError(innerHTML, enableButtons);
+    } else {
+        fetchTwitchUsername(auth.helixToken, auth.clientId);
+    }
+});
 
 // Wait for Twitch data.
 pityPointsDisplay.textContent = '...';
